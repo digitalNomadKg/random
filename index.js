eaaf4a966e358d82f263d8c29c0f625034107bec
Tim Kirves <tim.kirves@gmail.com>
2020-09-10 22:15:31 +0300

    js files update

---
+++ index.js
@@ -0,0 +1,37 @@
+// function(){
+//   var a="3";
+//   var b="8";
+
+//   var c = a;
+//   a=b;
+//   b=c;
+
+//   console.log("a is " + a);
+//   console.log("b is " + b);
+// }
+
+// var n = Math.random();
+// n=n*6;
+// n=Math.floor(n)+1;
+// console.log(n);
+
+//LoveCalculator
+
+// prompt("What is your name?");
+// prompt("What is their name?");
+
+// var loveScore= Math.random()*100;
+// loveScore = Math.floor(loveScore)+1;
+// if( loveScore>70){
+//   alert("Your score is " + loveScore + "%" + " you love each other");
+// }
+// if(loveScore>30 && loveScore<=70){
+//   alert("Your score is " + loveScore + "%");
+// }
+
+// if(loveScore<=30){
+//   alert("Your love score is " + loveScore + "%" + " ,you are like a dog and cat");
+// }
+
+
+
